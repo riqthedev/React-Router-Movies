675e4ad8bc9eddf20750615800756c6dee30ed50
riqthedev <riqthedev@gmail.com>
2021-08-17 01:26:00 -0400

    Finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route, Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,8 +14,9 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+          // Study this response with a breakpoint or log statements √
+          // and set the response data as the 'movieList' slice of state √
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -28,9 +31,24 @@ export default function App () {
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route>
+      <SavedList list={[ /* This is stretch */]} />
+      </Route>
+
+      <Route path={"/movies/:id"}>
+      <Movie/>
+      </Route>
+
+
+      <Route path="/">
+        <MovieList movies={movieList}/>
+      </Route>
+
+  
+
+
+
     </div>
   );
 }
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  const { id } = useParams();
+  
 
-  let id = 1;
+
+ 
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -13,13 +17,17 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+       setMovie(response.data)
+
+      
       })
       .catch(error => {
         console.error(error);
       });
+      
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
@@ -31,6 +39,7 @@ export default function Movie(props) {
   const { title, director, metascore, stars } = movie;
 
   return (
+    
     <div className="save-wrapper">
       <div className="movie-card">
         <h2>{title}</h2>
